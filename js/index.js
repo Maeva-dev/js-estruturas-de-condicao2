613cbd9a42116fe3017c7a11bb6635d46bf76dcf
Maeva-dev <maevacc.devel@gmail.com>
2024-10-09 15:33:07 -0300

    js-estruturas-de-condicao2

---
+++ js/index.js
@@ -0,0 +1,12 @@
+btn.onclick = function(){
+    if( 2 > 2){
+    }
+    switch (Number(mes.value)){
+        case value:
+            
+            break;
+    
+        default:
+            break;
+    }
+}
